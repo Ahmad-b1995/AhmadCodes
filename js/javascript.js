7c58ed67e90374df9cc6901a0958b88a33db479e
Ahmad <ahmad.b1995@gmail.com>
2021-06-01 20:40:30 +0430

    replay button fixed

--- js/javascript.js
+++ js/javascript.js
@@ -4,5 +4,15 @@ var replay_btn = document.getElementsByClassName('replay_btn')
 
 // when video ends event
 video.onended = function() {
-    replay_btn[0].style.visibility = 'visible'
+    replay_btn[0].style.opacity = '0.9'
+    replay_btn[0].style.transform = 'rotate(-360deg)'
 };
+
+// play the video again
+function play_again() {
+    if (video.paused) {
+        video.play()
+        replay_btn[0].style.opacity = '0'
+    }
+
+}
